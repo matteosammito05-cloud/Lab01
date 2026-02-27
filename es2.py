1c83e015c38ff35a23a7a48a6219a947c9d6ffed
matteosammito05-cloud <matteo.sammito05@gmail.com>
2026-02-27 17:32:09 +0100

    es2fatto

--- es2.py
+++ es2.py
@@ -4,9 +4,9 @@ s2 = set(input("Inserisci la seconda stringa: ").lower().strip().replace(" ", ""
 et=s1&s2
 print("Caratteri in entrambe le stringhe:", "".join(sorted(et)))
 
-ot1=s1^s2
-ot2=s2^s1
-ot=ot1+ot2
+ot=s1^s2
 print("Caratteri presenti in una ma non nell'altra:", "".join(sorted(ot)))
 
 abc=set("abcdefghijklmnopqrstuvwxyz")
+nots=abc-(s1|s2)
+print("Caratteri non presenti in nessuna delle due stringhe:", "".join(sorted(nots)))

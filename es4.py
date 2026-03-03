1209f4ad679acc1d784d336de1b40d8ba72c6256
matteosammito05-cloud <matteo.sammito05@gmail.com>
2026-03-03 12:04:47 +0100

    es4 fatto

---
+++ es4.py
@@ -0,0 +1,25 @@
+def factorial(n):
+    if n==0 or n==1:
+        return 1
+    else:
+        return n*factorial(n-1)
+
+def binomial(f1,f2,nk):
+    return f1/(f2*nk)
+n1=int(input("Primo numero: "))
+n2=int(input("Secondo numero: "))
+
+def main():
+    if n1==-1 and n2==-1:
+        print("Exit.")
+    else:
+        f1=factorial(n1)
+        f2=factorial(n2)
+        if n1>n2:
+            nk=factorial(n1-n2)
+        else:
+            nk=factorial(n2-n1)
+        print("Il coefficiente binomiale è: ",binomial(f1,f2,nk))
+
+main()
+
